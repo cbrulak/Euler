1acdc63aa99090280478fb889c8a5f61f2a89279
Chris Brulak <ccmbrulak@gmail.com>
2010-12-10 17:02:51 -0700

    updating problem 12

--- Problem12/Problem12.hs
+++ Problem12/Problem12.hs
@@ -9,26 +9,49 @@ secDiff :: ClockTime -> ClockTime -> Float
 secDiff (TOD secs1 psecs1) (TOD secs2 psecs2) = 
 	fromInteger (psecs2 - psecs1) / 1e12 + fromInteger (secs2 - secs1)
 
-generateTriangleNum :: Float -> Float
+divS n = [ x | x <- [1..(n)], n `rem` x == 0 ]    
+    
+generateTriangleNum :: Int -> Int
 generateTriangleNum n =
-    (n*n + n)/2
+    (n*n + n) `div` 2
 
-findNumOfDivisorsForTriangleNum :: Float -> Float
+findNumOfDivisorsForTriangleNum :: Int -> Int
 findNumOfDivisorsForTriangleNum numFactors = 
-    generateTriangleNum numFactors
-    
-    
+    length (divS (generateTriangleNum( numFactors)))
+
+findNumOfDivisorsForTriangleNumArr :: Int -> [Int]
+findNumOfDivisorsForTriangleNumArr numFactors = 
+    divS (generateTriangleNum( numFactors))
+
+findNumOfFactors :: Int -> Int -> Int
+findNumOfFactors numOfFactors num =
+    let i = (findNumOfDivisorsForTriangleNum num)
+    in
+   
+    if(numOfFactors == i)
+        then num
+    else
+        trace ("findNumOfFactors: numOfFactors " ++ show numOfFactors ++ " num is " ++ show num ++ " i is " ++ show i)
+        findNumOfFactors numOfFactors (num+1)
     
 main :: IO ()
 main = 
-	do 
-		args<-getArgs
-		t0 <- getClockTime
-		
-		let numOfDivisors = (read (head args))
-		
-		let triangle = findNumOfDivisorsForTriangleNum numOfDivisors
-		putStrLn ("Triangle number with " ++ show numOfDivisors ++ " num of divisors is " ++ show triangle )
-		t1 <- getClockTime
-		putStrLn ("time: " ++ show (secDiff t0 t1) ++ " seconds.")
+    do 
+        args<-getArgs
+        t0 <- getClockTime
+
+        let numOfDivisors = (read (head args))
+
+        --let triangle = findNumOfDivisorsForTriangleNum numOfDivisors
+        --let triangleArr = findNumOfDivisorsForTriangleNumArr numOfDivisors
+        --let triangle = length(triangleArr)
+
+        --putStrLn ("Triangle number with " ++ show numOfDivisors ++ " num of divisors is " ++ show triangle ++ show triangleArr)
+        
+        let firstTriangle = findNumOfFactors numOfDivisors 2
+        
+        putStrLn ("Triangle number is " ++ show firstTriangle)
+        
+        t1 <- getClockTime
+        putStrLn ("time: " ++ show (secDiff t0 t1) ++ " seconds.")
 	
